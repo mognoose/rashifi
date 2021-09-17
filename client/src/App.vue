14a3454a5764867d9901325df5d6c7ace67074d2
csaba <csaba.szilagyi@codetag.fi>
2021-09-17 15:06:27 +0300

    added client and some test functions

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <button @click="onTest('play')">Play</button>
+    <button @click="onTest('pause')">Pause</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      test: 'test',
+    }
+  },
+  methods: {
+    onTest(msg){
+      console.log(msg);
+      axios('http://localhost:3000/spotify/'+msg)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const express = require('express')
 const { exec } = require("child_process");
+const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+var corsOptions = {
+  origin: "*"
+}
+
+app.use(cors(corsOptions))
+
+app.get('/', (req, res, next) => {
   res.send('Hello World!')
-  exec("playerctl play", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+  let result = command("echo 'Hello World!'")
 })
+app.get('/spotify', (req, res) => {
+  command("spotify")
+  res.send('Spotify: Opened')
+})
+app.get('/spotify/:cmd', (req, res) => {
+  command("playerctl "+req.params.cmd)
+  res.send('Spotify: '+req.params.cmd)
+})
+app.get('/youtube/:url', (req, res) => {
+  command("brave-browser --app='https://www.youtube.com/watch?v='"+req.params.url)
+  res.send('Playing video on Youtube')
+})
+
+const command = function(cmd){
+  result = exec(cmd, (error, stdout, stderr) => {
+    if(error){console.log(`error: ${error.message}`);return;}
+    if(stderr){console.log(`stderr: ${stderr}`);return;}
+    console.log(`server: ${stdout}`);
+});
+}
 
 app.post('/', function (req, res) {
   res.send('Got a POST request')
